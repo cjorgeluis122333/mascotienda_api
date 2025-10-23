5e7fafce5cbf04fb98fdecbb3a5093fe2b8e360f
cjorgeluis122333 <cjorgeluis122333@gmail.com>
2025-10-23 08:27:50 -0400

    Add a util class for help me

---
+++ src/main/java/com/jorgeluis/mascotionda/common/utili/constant/CustomMessage.java
@@ -0,0 +1,7 @@
+package com.jorgeluis.mascotionda.common.utili.constant;
+
+public class CustomMessage {
+   public static final String USERNAME_OR_PASSWORD_INCORRECT = "The username or password are incorrect, place check the problem";
+    public static final String USERNAME_NOT_EXIST = "The username not exist";
+
+}
---
+++ src/main/java/com/jorgeluis/mascotionda/controller/security/util/logout/JwtUtils.java
@@ -0,0 +1,74 @@
+package com.jorgeluis.mascotionda.controller.security.util.logout;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jorgeluis.mascotionda.controller.security.SimpleGrantedAuthorityJsonCreator;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+
+import static com.jorgeluis.mascotionda.controller.security.util.auth.TokenJwtConfig.SECRET_KEY;
+
+
+@Component
+public class JwtUtils {
+    private final ObjectMapper mapper;
+
+    public JwtUtils() {
+        this.mapper = new ObjectMapper()
+                .addMixIn(SimpleGrantedAuthority.class, SimpleGrantedAuthorityJsonCreator.class)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    /**
+     * Extrae y devuelve la fecha de expiración (exp) del JWT.
+     * @param token
+     * @return
+     */
+    public Date getExpirationDateFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(SECRET_KEY)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return claims.getExpiration();
+    }
+
+    /**
+     * Lee el subject (username) del JWT.
+     *
+     * Extrae el claim "authorities" y lo deserializa como lista de SimpleGrantedAuthority.
+     *
+     * Devuelve un objeto UsernamePasswordAuthenticationToken, que es el que Spring Security usa internamente.
+     * @param token
+     * @return
+     */
+    public Authentication getAuthentication(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(SECRET_KEY)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        String username = claims.getSubject();
+        byte[] authBytes = claims.get("authorities").toString().getBytes();
+        Collection<? extends GrantedAuthority> authorities = null;
+        try {
+            authorities = Arrays.asList(mapper.readValue(authBytes, SimpleGrantedAuthority[].class));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new UsernamePasswordAuthenticationToken(username, null, authorities);
+    }
+}
+
